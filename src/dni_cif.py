26d7f703364ae8aaa9a72a5ca35e6008e8023ac0
jrgeepd <pazosdominguez8@gmail.com>
2026-02-09 13:24:29 +0100

    feat: acabar las dos clases
    Co-authored-by: tomy :) <T0T11@users.noreply.github.com>

---
+++ src/dni_cif.py
@@ -0,0 +1,58 @@
+class Dni:
+    def __init__(self):
+        self.dni = ""
+        self.numeroSano = None
+        self.letraSana = None
+
+    def getDni(self):
+        return self.dni
+    
+    def setDni(self, dni):
+        self.dni = dni
+
+    def getNumeroSano(self):
+        return self.numeroSano
+    
+    def getLetraSana(self):
+        return self.letraSana
+    
+    def checkCIF(self):
+        if not self.dni or len(self.dni) < 2:
+            return False
+        
+        letra = self.dni[-1]
+
+        if letra in "IOUÑ": # Letras prohibidas en el CIF
+            return False
+        return True 
+
+    def checkDni(self): 
+        if len(self.dni) != 9:
+            return False
+        numero = self.dni[:-1]
+        letra = self.dni[-1]
+        if not numero.isdigit():
+            return False
+        self.numeroSano = int(numero)
+        self.letraSana = "TRWAGMYFPDXBNJZSQVHLCKE"[self.numeroSano % 23]
+        return True
+    
+    def checkLetra(self):
+        if not hasattr(self, "letraSana"):
+            return False
+        return self.letraSana == self.dni[-1]
+    
+    def obtenerLetra(self):
+        if not hasattr(self, "letraSana"):
+            return None
+        return self.letraSana
+    
+    def getParteAlfabeticaDni(self):
+        return self.dni[-1]
+    
+    def getParteNumericaDni(self):
+        if not getattr(self, "numeroSano", False):
+            return ""
+        return self.dni[:-1]
+    
+
---
+++ src/tablaAsignacion.py
@@ -0,0 +1,45 @@
+class TablaAsignacion:
+    def getTabla(self):
+        return [
+            "T",
+            "R",
+            "W",
+            "A",
+            "G",
+            "M",
+            "Y",
+            "F",
+            "P",
+            "D",
+            "X",
+            "B",
+            "N",
+            "J",
+            "Z",
+            "S",
+            "Q",
+            "V",
+            "H",
+            "L",
+            "C",
+            "K",
+            "E",
+        ]
+    
+    def getLetra(self, posicion):
+        tabla = self.getTabla()
+        if 0 <= posicion < len(tabla):
+            return tabla[posicion]
+        else:
+            return "Posicion letra fuera de rango"
+        
+    def getModulo(self):
+        return 23
+    
+    def isLetraPermitida(self, letra):
+        return letra in self.getTabla()
+    
+    def calcularLetra(self, numero_dni):
+        modulo = self.getModulo()
+        posicion = int(numero_dni) % modulo
+        return self.getLetra(posicion)
